2d19d0ed2ff687e0d9889d6927df7728b43f881a
Maiko <ewigemagie@outlook.com>
2025-06-05 23:40:07 -0300

    feat: create views

--- Database/Views.sql
+++ Database/Views.sql
@@ -1,7 +1,17 @@
 CREATE VIEW vw_livros_mais_vendidos AS
-SELECT Livros.nome, Itens_Pedido.quantidade 
-FROM Itens_Pedido 
-JOIN Livros on Itens_Pedido.livro_id=Livros.id DESC LIMIT 10;
+SELECT lv.titulo as Titulo_Livro, SUM(ip.quantidade) AS Total_Vendido
+FROM Itens_Pedido AS ip
+JOIN Livros as lv on ip.livro_id=lv.id DESC LIMIT 10
+GROUP BY lv.id, lv.titulo
+ORDER BY Total_Vendido DESC;
+
+CREATE VIEW vw_estoque_baixo AS
+SELECT id as ID_Livro, titulo as Titulo_Livro, quantidade as Quantidade_Estoque
+FROM Livros
+WHERE quantidade_estoque < 10;
 
 CREATE VIEW vw_clientes_ativos AS
-SELECT 
+SELECT DISTINCT c.id AS ID_Cliente, c.nome AS Nome_Cliente, c.email AS Email
+FROM Clientes AS c
+JOIN Pedidos AS p ON c.id=p.cliente_id
+WHERE p.data_pedido >= DATE('now', '-6 months');
